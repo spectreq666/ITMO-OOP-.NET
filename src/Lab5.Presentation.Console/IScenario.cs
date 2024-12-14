e8d39da60e28dce91521c93addc34ba4494b6848
spectreq <alesha.malinin@gmail.com>
2024-12-14 09:23:39 +0300

    refactor: fix architecture

--- src/Lab5.Presentation.Console/IScenario.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Lab5.Presentation.Console;
-
-public interface IScenario
-{
-    string Name { get; }
-
-    void Run();
-}
--- src/Lab5.Presentation.Console/IScenarioProvider.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-namespace Lab5.Presentation.Console;
-
-public interface IScenarioProvider
-{
-    bool TryGetScenario([NotNullWhen(true)] out IScenario? scenario);
-}
